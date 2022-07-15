47a90950f2e9af27e4d7649cf2a96fdae4d5b620
Ganesh <61945621+Ganesh-Murugan@users.noreply.github.com>
2022-07-15 12:31:17 +0530

    Add files via upload

---
+++ Practice/Practice/ContactCell.swift
@@ -0,0 +1,29 @@
+//
+//  ContactCell.swift
+//  Practice
+//
+//  Created by zoho on 10/06/22.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+    @IBOutlet var name : UILabel?
+    @IBOutlet var number : UILabel?
+    @IBOutlet var emailId: UILabel?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        print(contentView.bounds.width)
+            
+    }
+    func add(_ index: Contact)  {
+        name?.text = index.name
+        number?.text = index.number?.toString
+        emailId?.text = index.emailId
+    }
+    
+}
---
+++ Practice/Practice/extensions.swift
@@ -0,0 +1,55 @@
+//
+//  extensions.swift
+//  Practice
+//
+//  Created by zoho on 10/06/22.
+//
+
+import Foundation
+
+extension Int {
+    var toString: String  {
+        String(self)
+    }
+}
+
+extension Optional {
+    var unwrap: String {
+        guard let self = self else { return "Unknown" }
+        return self as! String
+    }
+    var intUnwrap: Int {
+        guard let self = self else { return 0 }
+        return self as! Int
+    }
+}
+
+extension Array where Element == Contact {
+    func searchFilter(searchText: String) -> [Contact] {
+        return self.filter { searchText.isEmpty || (($0.name.unwrap).localizedStandardContains(searchText)) ||
+            (($0.number?.toString).unwrap).localizedStandardContains(searchText)                                    }
+    }
+
+    func sort<T: Comparable>(_ property: (Element) -> T) -> [Element] {
+        return self.sorted(by: {property($0) < property($1)})
+    }
+}
+
+extension Array {
+    func unique(_ selector:(Element,Element)->Bool) -> Array<Element> {
+        return reduce(Array<Element>()){
+            if let last = $0.last {
+                return selector(last,$1) ? $0 : $0 + [$1]
+            } else {
+                return [$1]
+            }
+        }
+    }
+}
+//
+//extension Cell {
+//    func addCell(_ index: Settings) {
+//        self.settingName.text = index.settingName
+//        self.settingDescription.text = index.settingDescription
+//    }
+//}
